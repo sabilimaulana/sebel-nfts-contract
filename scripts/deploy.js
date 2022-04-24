213129ab10ba2ae5e4db1dfa55e566660b604a5b
sabilimaulana <spacysbl.learn@gmail.com>
2022-04-24 19:49:45 +0700

    done: Section 1

---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+const main = async () => {
+  const [deployer] = await hre.ethers.getSigners();
+
+  const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+
+  let txn = await nftContract.makeAnEpicNFT();
+  await txn.wait();
+
+  txn = await nftContract.makeAnEpicNFT();
+  await txn.wait();
+
+  console.log("Deployer:", deployer.address);
+  console.log("Contract deployed to:", nftContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
